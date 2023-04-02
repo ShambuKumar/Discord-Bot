51d95b20066b7b2324143a378db65cb0c7f0eec4
Shambu Kumar <kumarshambu2010@gmail.com>
2023-04-02 16:54:43 +0200

    server info command

--- commands/util/server.js
+++ commands/util/server.js
@@ -5,8 +5,67 @@ module.exports = {
 		.setName('server')
 		.setDescription('Display info about this server.'),
 	async execute(interaction) {
-		return interaction.reply(
-			`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`,
-		);
+		const guild = interaction.guild;
+		function createEmbed() {
+			const embed = {
+				title: `${guild.name}`,
+				color: 0x0099ff,
+				thumbnail: {
+					url: `${guild.iconURL()}`,
+				},
+				fields: [
+					{
+						name: 'Server ID',
+						value: `${guild.id}`,
+					},
+					{
+						name: 'Owner',
+						value: `${guild.members.cache.get(guild.ownerId)}`,
+					},
+					{
+						name: 'Created',
+						value: `${guild.createdAt}`,
+					},
+					{
+						name: 'Members',
+						value: `${guild.members.cache.map((m) => m).join(' ')}`,
+					},
+					{
+						name: 'Roles',
+						value: `${guild.roles.cache
+							.map((r) => r)
+							.join(' ')
+							.replace(/@everyone/g, '')}`,
+					},
+					{
+						name: 'Channels',
+						// Only get ThreadChannel objects
+						value: `${guild.channels.cache
+							.filter((c) => c.type != 4)
+							.map((c) => c)
+							.join(' ')}`,
+					},
+					{
+						name: 'Boosts',
+						value: `${guild.premiumSubscriptionCount}`,
+					},
+					{
+						name: 'Bots',
+						value: `${guild.members.cache.filter((m) => m.user.bot).size}`,
+					},
+					{
+						name: 'Online Members',
+						value: `${
+							interaction.guild.members.cache.filter(
+								(member) => member.presence?.status == 'online',
+							).size
+						}`,
+					},
+				],
+				timestamp: new Date(),
+			};
+			return embed;
+		}
+		return interaction.reply({ embeds: [createEmbed()] });
 	},
 };
--- commands/util/user.js
+++ commands/util/user.js
@@ -1,44 +1,60 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
 		.setDescription('Provides information about the user.')
 		.addUserOption((option) =>
-			option.setName('target').setDescription('The user\'s info you want to see'),
+			option.setName('target').setDescription('The user\'s info to show'),
 		),
 
 	async execute(interaction) {
-		const userEmbed = createEmbed();
-		const user = interaction.options.getUser('target').fetch(true);
-
-		function removeValue(value, index, arr) {
-			// If the value at the current array index matches the specified value (2)
-			if (value === '@everyone') {
-				// Removes the value from the original array
-				arr.splice(index, 1);
-				return true;
-			}
-			return false;
-		}
-		user.filter(removeValue);
+		const user = interaction.options.getUser('target');
 		const member = interaction.guild.members.cache.get(user.id);
-		const roles = member.roles.cache.map((r) => `${r}`).join(' , ');
+		// Exclue the @everyone role
+		const roles = member.roles.cache
+			.map((r) => r)
+			.join(' ')
+			.replace(/@everyone/g, '');
+		const userEmbed = createEmbed();
 		// interaction.member is the GuildMember object, which represents the user in the specific guild
 		function createEmbed() {
-			const embed = new EmbedBuilder()
-				.setTitle('User Info')
-				.setColor(user.hexAccentColor)
-				.setThumbnail(user.displayAvatarURL())
-				.addField('Username', user.username, true)
-				.addField('ID', user.id, true)
-				.addField('Created At', user.createdAt, true)
-				.addField('Joined At', member.joinedAt, true)
-				.addField('Roles', roles, true)
-				.addField('Bot', user.bot, true)
-				.setTimeStamp();
+			const embed = {
+				title: `${user.username}`,
+				color: member.displayColor,
+				thumbnail: {
+					url: `${user.displayAvatarURL()}`,
+				},
+				fields: [
+					{
+						name: 'User ID',
+						value: `${user.id}`,
+					},
+					{
+						name: 'Nickname',
+						value: `${member.nickname || 'None'}`,
+					},
+					{
+						name: 'Joined',
+						value: `${member.joinedAt}`,
+					},
+					{
+						name: 'Registered',
+						value: `${user.createdAt}`,
+					},
+					{
+						name: 'Roles',
+						value: `${roles}`,
+					},
+					{
+						name: 'Status',
+						value: `${member.presence?.status}`,
+					},
+				],
+				timestamp: new Date(),
+			};
 			return embed;
 		}
-		await interaction.reply(userEmbed);
+		await interaction.reply({ embeds: [userEmbed] });
 	},
 };
